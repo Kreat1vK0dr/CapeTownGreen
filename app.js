09e4f47dff145ecfb5b117ca1d15ab33ae2943f4
Daniel Maartens <daniel@projectcodex.co>
2016-02-17 12:20:30 +0200

    Completed keyCodeName task

--- app.js
+++ app.js
@@ -2,8 +2,17 @@
 //find the body tag and store it in a variable called 'body'
 var body = document.querySelector("body");
 
-//listen for the keydown event
+//listen for the keydown event(
 body.onkeydown = function(e){
-    //e.keyCode - will capture the key codes
+    //e.keyCode - will capture the key code
     //displayMessage(e.keyCode);
 };
+var keyCodeName = function(keyCode) {
+    switch (keyCode) {
+      case 37: return "left";
+      case 38: return "up";
+      case 39: return "right";
+      case 40: return "down";
+      default: return "";
+      }
+    };
