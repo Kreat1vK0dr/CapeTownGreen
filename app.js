36adfb6fda707a303b1ced2f12c9723b95856103
Daniel Maartens <daniel@projectcodex.co>
2016-02-18 11:13:29 +0200

    Solved moving taxi code with Codex method. Previous commit solved it using my own method.

--- app.js
+++ app.js
@@ -3,47 +3,6 @@
 var body = document.querySelector("body");
 //adding function displayMessage (copied from support.js)
 
-
-var moveTaxi = function(from, to) {
-  var taxi = document.querySelector('.taxi');
-    taxi.classList.remove(from);
-    taxi.classList.add(to);
-};
-
-
-var num_words = ["slot-one-of-nine","slot-two-of-nine", "slot-three-of-nine", "slot-four-of-nine", "slot-five-of-nine", "slot-six-of-nine", "slot-seven-of-nine", "slot-eight-of-nine", "slot-nine-of-nine"];
-/*
-var getTaxiClasss = function (currentIndex) {
-    var num_words = ["slot-one-of-nine","slot-two-of-nine", "slot-three-of-nine", "slot-four-of-nine", "slot-five-of-nine", "slot-six-of-nine", "slot-seven-of-nine", "slot-eight-of-nine", "slot-nine-of-nine"];
-    return num_words[currentIndex - 1];
-};
-*/
-
-var taxiLocationCounter = 1;
-//listen for the keydown event(
-body.onkeydown = function(e){
-    if (e.keyCode === 39) {
-        if (taxiLocationCounter > 0 && taxiLocationCounter < 9) {
-          taxiLocationCounter += 1;
-          moveTaxi(num_words[taxiLocationCounter-2],num_words[taxiLocationCounter-1]);
-
-        } else {
-            taxiLocationCounter = 1;
-            moveTaxi(num_words[num_words.length - 1], num_words[0]);
-        }
-
-     } else if (e.keyCode === 37) {
-        if (taxiLocationCounter===1) {
-              taxiLocationCounter = num_words.length;
-              moveTaxi(num_words[0],num_words[num_words.length-1]);
-            } else if (taxiLocationCounter>1 && taxiLocationCounter<10) {
-                  taxiLocationCounter -= 1;
-                  moveTaxi(num_words[taxiLocationCounter],num_words[taxiLocationCounter-1]);
-            }
-        }
-      displayMessage(keyCodeName(x) /*taxiLocationCounter*/);
-    };
-
 var keyCodeName = function(keyCode) {
     switch (keyCode) {
       case 37: return "left";
@@ -53,3 +12,73 @@ var keyCodeName = function(keyCode) {
       default: return "";
       }
     };
+
+var position = ["slot-one-of-nine","slot-two-of-nine", "slot-three-of-nine", "slot-four-of-nine", "slot-five-of-nine", "slot-six-of-nine", "slot-seven-of-nine", "slot-eight-of-nine", "slot-nine-of-nine"];
+
+
+
+var taxiLocationCounter = 1;
+
+var createLocactionClass = function(x) {
+    switch (x) {
+      case 1: return position[0];
+      case 2: return position[1];
+      case 3: return position[2];
+      case 4: return position[3];
+      case 5: return position[4];
+      case 6: return position[5];
+      case 7: return position[6];
+      case 8: return position[7];
+      case 9: return position[8];
+      default: return "";
+    }
+};
+
+var moveTaxi = function(from, to) {
+  var taxi = document.querySelector('.taxi');
+    taxi.classList.remove(from);
+    taxi.classList.add(to);
+};
+
+var moveForward = function() {
+  var currentLocation = createLocactionClass(taxiLocationCounter);
+  taxiLocationCounter++;
+  var newLocation = createLocactionClass(taxiLocationCounter);
+  moveTaxi(currentLocation, newLocation);
+  };
+
+var moveBackward = function() {
+  var currentLocation = createLocactionClass(taxiLocationCounter);
+  taxiLocationCounter--;
+  var newLocation = createLocactionClass(taxiLocationCounter);
+  moveTaxi(currentLocation, newLocation);
+};
+
+
+/*
+var getTaxiClasss = function (currentIndex) {
+    var position = ["slot-one-of-nine","slot-two-of-nine", "slot-three-of-nine", "slot-four-of-nine", "slot-five-of-nine", "slot-six-of-nine", "slot-seven-of-nine", "slot-eight-of-nine", "slot-nine-of-nine"];
+    return position[currentIndex - 1];
+};
+*/
+
+
+//listen for the keydown event(
+body.onkeydown = function(e){
+  if (e.keyCode === 39) {
+    if (taxiLocationCounter > 0 && taxiLocationCounter < 9) {
+      moveForward();
+        } else {
+            taxiLocationCounter = 1;
+            moveTaxi(position[position.length - 1], position[0]);
+          }
+    } else if (e.keyCode === 37) {
+        if (taxiLocationCounter > 1 && taxiLocationCounter < 10) {
+       moveBackward();
+     } else {
+       taxiLocationCounter = 9;
+        moveTaxi(position[0],position[8]);
+     }
+    }
+    displayMessage(createLocactionClass(taxiLocationCounter)/*keyCodeName(x)*/ /*taxiLocationCounter*/);
+};
