1462d1f5d1002d882860affd4128b1ee33584bed
Daniel Maartens <daniel@projectcodex.co>
2016-02-21 00:28:58 +0200

    Made some changes all around to lay the foundations of a 'game'.

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+var randomNumber = function(range){
+  return Math.floor(Math.random() * range);
+};
+
+var moneyIndex = {
+    0 : 5,
+    1 : 10,
+    2 : 20
+  };
+
+var passengerList = function(){
+
+  var maxNumber = randomNumber(40);
+  var passengers = [];
+  for(var i=0;i<maxNumber;i++){
+    var random = randomNumber(3) ;
+    passengers.push(moneyIndex[random]);
+  }
+
+  return passengers;
+};
+
 
 //find the body tag and store it in a variable called 'body'
 var body = document.querySelector("body");
@@ -83,11 +105,11 @@ return position[currentIndex - 1];
 var test = true;
 
 function TrafficLight(x) {
-  console.log(x);
+  // console.log(x);
   var className = createTrafficLightClass(x);
-  console.log(className);
+  // console.log(className);
   var trafficLightElement = document.querySelector(className);
-  console.log(trafficLightElement.classList);
+  // console.log(trafficLightElement.classList);
   test = trafficLightElement.classList;
   this.makeGreen = function () {
     trafficLightElement.classList.remove("lights-stop");
@@ -154,7 +176,6 @@ var lightposition = function(object, x) {
     case 3 : object.makeRed();
     break;
     default: return false;
-    break;
   }
 };
 
@@ -185,63 +206,268 @@ var resetLightCounter = function(x) {
     lightcolor = 3;
   }
 };
+var elementQFirst = document.querySelector('.queue-first');
+var elementQSecond = document.querySelector('.queue-second');
+var elementQThird = document.querySelector('.queue-third');
+
+// console.log(document.querySelector('.queue-first').classList);
+
+function changeColorFive() {
+  elementQFirst.classList.add('active');
+  elementQFirst.classList.remove('inactive');
+  elementQSecond.classList.remove('active');
+  elementQSecond.classList.add('inactive');
+  elementQThird.classList.remove('active');
+  elementQThird.classList.add('inactive');
+  console.log(document.querySelector('.queue-first').classList);
+  console.log(document.querySelector('.queue-second').classList);
+  console.log(document.querySelector('.queue-third').classList);
+}
+function changeColorTen() {
+  elementQSecond.classList.add('active');
+  elementQSecond.classList.remove('inactive');
+  elementQFirst.classList.remove('active');
+  elementQFirst.classList.add('inactive');
+  elementQThird.classList.remove('active');
+  elementQThird.classList.add('inactive');
+  console.log(document.querySelector('.queue-first').classList);
+  console.log(document.querySelector('.queue-second').classList);
+  console.log(document.querySelector('.queue-third').classList);
+}
+function changeColorTwenty() {
+  elementQThird.classList.add('active');
+  elementQThird.classList.remove('inactive');
+  elementQSecond.classList.remove('active');
+  elementQSecond.classList.add('inactive');
+  elementQFirst.classList.remove('active');
+  elementQFirst.classList.add('inactive');
+  console.log(document.querySelector('.queue-first').classList);
+  console.log(document.querySelector('.queue-second').classList);
+  console.log(document.querySelector('.queue-third').classList);
+}
+function changeColorBody() {
+  elementQFirst.classList.remove('active');
+  elementQFirst.classList.add('inactive');
+  elementQSecond.classList.remove('active');
+  elementQSecond.classList.add('inactive');
+  elementQThird.classList.remove('active');
+  elementQThird.classList.add('inactive');
+  console.log(document.querySelector('.queue-first').classList);
+  console.log(document.querySelector('.queue-second').classList);
+  console.log(document.querySelector('.queue-third').classList);
+}
+
+// console.log(document.getElementById('queue-ten-rand').classList);
+
+var displayQueueCountFive = function(val){
+    var q = document.getElementById('queue-five-rand');
+    q.innerHTML = val;
+};
+var displayQueueCountTen = function(val){
+    var q = document.getElementById('queue-ten-rand');
+    q.innerHTML = val;
+};
+var displayQueueCountTwenty = function(val){
+    var q = document.getElementById('queue-twenty-rand');
+    q.innerHTML = val;
+};
+
+var displayQueues = function (array) {
+  if (five===true) {
+  displayQueueCountFive(array[0]);
+  fiveCount = array[0];
+  fiveRem = array[0];
+
+} else {
+  displayQueueCountFive(0);
+  fiveCount = 0;
+  fiveRem = 0;
+  }
+
+if (ten===true) {
+  if (five===true) {
+  displayQueueCountTen(array[1]);
+  tenCount = array[1];
+  tenRem = array[1];
+} else {
+  displayQueueCountTen(array[0]);
+  tenCount = array[0];
+  tenRem = array[0];
+}
+} else if (five===true){displayQueueCountTen(0);
+  tenCount = 0;
+  tenRem = 0;
+}
+
+if (twenty===true) {
+  if (ten===true && five===true) {
+    displayQueueCountTwenty(array[2]);
+    twentyCount = array[2];
+    twentyRem = array[2];
+  } else {
+      displayQueueCountTwenty(array[1]);
+      twentyCount = array[1];
+      twentyRem = array[1];
+  }
+} else {
+  displayQueueCountTwenty(0);
+  twentyCount = 0;
+  twentyRem = 0;
+}
+console.log(fiveCount);
+console.log(tenCount);
+console.log(twentyCount);
+};
+
+var five;
+var ten;
+var twenty;
+var fiveCount;
+var tenCount;
+var twentyCount;
+var fiveRem;
+var tenRem;
+var twentyRem;
+
+
+var createQueues = function () {
+  var queueCount = [];
+  var passengers = passengerList().sort(function(a,b){return a-b;});
+  var prev =0;
+  for (i=0;i<passengers.length;i++) {
+    if (passengers[i] !== prev) {
+      queueCount.push(1);
+      if (passengers[i]===5) {
+        five = true;
+      } else if (passengers[i]===10) {
+        ten = true;
+      } else if (passengers[i]===20) {
+        twenty = true;
+      }
+      } else {
+      queueCount[queueCount.length-1]++;
+    } prev = passengers[i];
+
+  //  displayQueues(queueCount);
+} console.log(queueCount);
+displayQueues(queueCount);
+};
+
+
+var displayTaxiPassengerCountFive = function(count){
+    var q = document.querySelector(".five-rand");
+    q.innerHTML = count;
+};
+var displayTaxiPassengerCountTen = function(count){
+    var q = document.querySelector(".ten-rand");
+    q.innerHTML = count;
+};
+var displayTaxiPassengerCountTwenty = function(count){
+    var q = document.querySelector(".twenty-rand");
+    q.innerHTML = count;
+};
+
+// displayMessage("Press R to randomly fill the queue with passengers");
+var taxiCountFive =0;
+var taxiCountTen = 0;
+var taxiCountTwenty = 0;
+
+console.log(fiveRem);
+
+
 
 body.onkeydown = function(e){
-  console.log(redLightBehind());
+  // console.log(redLightBehind());
+  // console.log(document.querySelector('.taxi').classList
+
   var trafficLight = new TrafficLight(taxiLocationCounter);
-  // var locationBehind = taxiLocationCounter - 1;
-  // var lightBehind = null;
-  //
-  // if (locationBehind > 0){
-  //   lightBehind = new TrafficLight(locationBehind)
-  // }
+if(e.keyCode===82) {
+createQueues();
+
+  displayMessage(e.keyCode);
+} else {
+  displayMessage("error");
+}
+
+if (document.querySelector('.taxi').classList.contains('slot-one-of-nine')) {
+  if (elementQFirst.classList.contains('active')) {
+    if (e.keyCode===107 && fiveCount>0) {
+    taxiCountFive++;
+    fiveCount--;
+    displayTaxiPassengerCountFive(taxiCountFive);
+    displayQueueCountFive(fiveCount);
+    console.log(fiveRem);
+  } if (e.keyCode===109 && fiveCount<fiveRem) {
+    taxiCountFive--;
+    fiveCount++;
+    displayTaxiPassengerCountFive(taxiCountFive);
+    displayQueueCountFive(fiveCount);
+  }
+} else if (elementQSecond.classList.contains('active')) {
+  if (e.keyCode===107 && tenCount>0) {
+  taxiCountTen++;
+  tenCount--;
+  displayTaxiPassengerCountTen(taxiCountTen);
+  displayQueueCountTen(tenCount);
+  console.log(twentyRem);
+} if (e.keyCode===109 && tenCount<tenRem) {
+  taxiCountTen--;
+  tenCount++;
+  displayTaxiPassengerCountTen(taxiCountTen);
+  displayQueueCountTen(tenCount);
+}
+  } else if (elementQThird.classList.contains('active')) {
+    if (e.keyCode===107 && twentyCount>0) {
+    taxiCountTwenty++;
+    twentyCount--;
+    displayTaxiPassengerCountTwenty(taxiCountTwenty);
+    displayQueueCountTwenty(twentyCount);
+    console.log(twentyRem);
+  } if (e.keyCode===109 && twentyCount<twentyRem) {
+    taxiCountTwenty--;
+    twentyCount++;
+    displayTaxiPassengerCountTwenty(taxiCountTwenty);
+    displayQueueCountTwenty(twentyCount);
+  }
+}
+}
 
   if (e.keyCode===38) {
     if (lightcolor<3) {
       lightcolor++;
       lightposition(trafficLight, lightcolor);
-    // else
-    //   lightposition(trafficLight, lightcolor);
-    //
   }
 } else if (e.keyCode===40) {
       if (lightcolor>1){
     lightcolor--;
     lightposition(trafficLight, lightcolor);
-    // trafficLight.makeGreen();
     }
   } else if (e.keyCode === 39) {
+
 
       if (taxiLocationCounter < 9) {
         if(canIMoveForward(trafficLight)===true) {
         moveForward();
-        console.log(test);
+        // console.log(test);
         resetLightCounter(taxiLocationCounter);
-      } else {
-        taxiLocationCounter = taxiLocationCounter;
       }
-    } else {
+    } else if (canIMoveForward(trafficLight)===true) {
         taxiLocationCounter = 1;
         moveTaxi(position[position.length - 1], position[0]);
-
       }
     } else if (e.keyCode === 37) {
 
       if (taxiLocationCounter > 1) {
         if (canIMoveBackward()===true) {
-          console.log(redLightBehind());
-
          moveBackward();
          resetLightCounter(taxiLocationCounter);
-       } else {
-              taxiLocationCounter = taxiLocationCounter;
-       }
-      } else {
-          taxiLocationCounter = 9;
-          moveTaxi(position[0],position[8]);
-
-        }
       }
-      console.log(test);
-      displayMessage(createLocationClass(taxiLocationCounter)+" "+lightcolor;//createTrafficLightClass(taxiLocationCounter) //createLocationClass(taxiLocationCounter) /*keyCodeName(x)*/ )//taxiLocationCounter //lightcolor);
+    } else if (canIMoveBackward()===true) {
+      taxiLocationCounter = 9;
+      moveTaxi(position[0],position[8]);
+    }
+  }
+  console.log(document.querySelector('.taxi').classList);
+      // console.log(test);
+      // displayMessage(createLocationClass(taxiLocationCounter)+" "+lightcolor);//createTrafficLightClass(taxiLocationCounter) //createLocationClass(taxiLocationCounter) /*keyCodeName(x)*/ )//taxiLocationCounter //lightcolor);
 };
--- support.js
+++ support.js
@@ -1,47 +1,35 @@
-var randomNumber = function(range){
-  return Math.floor(Math.random() * range);
-};
 
-var moneyIndex = {
-    0 : 5,
-    1 : 10,
-    2 : 20
-  };
 
-var passengerList = function(maxPassengers){
+  // console.log(passengers);
+  // console.log(passengerCount);
 
-  var maxNumber = randomNumber(40);
-  var passengers = [];
-  for(var i=0;i<maxNumber;i++){
-    var random = randomNumber(3) ;
-    passengers.push(moneyIndex[random]);
-  }
+// };
+// allocatePassengers();
 
-  return passengers;
-};
+// console.log(passengerList());
+// console.log(passengerList().length);
 
-var taxiPriceType = function(){
-  var random = randomNumber(3);
-  return moneyIndex[random];
-};
+// var taxiPriceType = function(){
+//   var random = randomNumber(3);
+//   return moneyIndex[random];
+// };
 
-var displayMessage = function(text){
-    var message = document.getElementById("message");
-    message.innerHTML = text;
-};
+var changeTaxiPassengers = function (array) {
+  displayTaxiPassengerCountFive(array[0]);
+  displayTaxiPassengerCountTen(array[1]);
+  displayTaxiPassengerCountTwenty(array[2]);
+}
 
-var displayTaxiPassengerCount = function(count){
-    var q = document.querySelector(".passengers");
-    q.innerHTML = count;
-};
 
-var displayQueueCount = function(val){
-    var q = document.querySelector("#queue-five-rand");
-    q.innerHTML = val;
-};
+
 
 var moveTaxi = function(from, to) {
     var taxi = document.querySelector('.taxi');
     taxi.classList.remove(from);
     taxi.classList.add(to);
+};
+
+var displayMessage = function(text){
+    var message = document.getElementById("message");
+    message.innerHTML = text;
 };
